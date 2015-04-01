953df6260074baa12017c8ea95fea972697cb406
Keith Turgeon <keith.turgeon@live.com>
2015-04-01 13:26:39 -0400

    Continued with next lecture

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,11 +3,10 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
-	public int maxHits;
 	public Sprite[] hitSprites;
 
-	private int timesHit;
 	private LevelManager levelManager;
+	private int timesHit;
 
 	// Use this for initialization
 	void Start () {
@@ -21,19 +20,29 @@ public class Brick : MonoBehaviour {
 	}
 		
 	void OnCollisionEnter2D (Collision2D collision) {
-		print ("hit!");
+		bool isBreakable = (this.tag == "Breakable");
+		if (isBreakable) {
+			HandleHits ();
+		}
+	}	
+
+	void HandleHits() {
 		timesHit++;
+		int maxHits = hitSprites.Length + 1;
+		
 		if (timesHit >= maxHits) {
 			Destroy (gameObject);
 		} else {
 			LoadSprites();
 		}
+	}
 
-	}	
 
 	void LoadSprites() {
 		int spriteIndex = timesHit - 1;
-		this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		if (hitSprites [spriteIndex]) {
+			this.GetComponent<SpriteRenderer> ().sprite = hitSprites [spriteIndex];
+		}
 	}
 
 
